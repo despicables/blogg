e78a77055f946089687967291b33d09dd93329a2
rojesh <rojesh.shrestha@sproutify.com>
2012-12-04 23:37:59 +0545

    validation addition in example

---
+++ railsApp/rails/locationapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locationapp::Application.config.secret_token = 'a2536d6a2d8e8c90faa89370e41186d792874a7375d2fd1cef2d89a0c98fa3bb3610dac5c8c2e889d717b436b5d7714494da7ba27998e4dbc3ff0a47e8d6812c'
